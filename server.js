c5c1327490609dd42e658d56598dd383ec0d4c9e
Sujan Chhetri <44917558+sujanchhetri@users.noreply.github.com>
2020-04-08 17:53:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,37 +1,33 @@
-var express  = require('express');
-var app      = express();
-var port     = process.env.PORT || 8000;
-var mongoose = require('mongoose');
-var passport = require('passport');
-var flash    = require('connect-flash');
+const express  = require('express');
+const app      = express();
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash    = require('connect-flash');
 
-var morgan       = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser   = require('body-parser');
-var session      = require('express-session');
-
- // connect to our database
-mongoose.connect('mongodb://localhost/blog', {
-                    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+const cookieParser = require('cookie-parser');
+const bodyParser   = require('body-parser');
+const session      = require('express-session');
+const morgan       = require('morgan');
+mongoose.connect('mongodb://localhost/databasename', {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
 })
-// pass passport for configuration
 
-// set up our express application
-app.use(morgan('dev')); // log every request to the console
-app.use(cookieParser()); // read cookies (needed for auth)
-app.use(bodyParser()); // get information from html forms
-
-app.set('view engine', 'ejs'); // set up ejs
-
-// required for passport
-app.use(session({ secret: 'ilovenodejs' })); // session secret
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
-app.use(flash()); // use connect-flash for flash messages stored in session
-
-// routes 
-require('./app/routes.js')(app, passport); 
 require('./config/passport')(passport); 
 
 
-app.listen(port);
+
+app.use(morgan('dev')); 
+app.use(cookieParser()); 
+app.use(bodyParser()); 
+
+app.set('view engine', 'ejs'); 
+app.use(session({ secret: 'nodeisthebest' })); 
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash()); 
+
+
+
+require('./app/routes.js')(app, passport);
+
+app.listen(8000)
